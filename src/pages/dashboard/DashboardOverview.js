f72f8837c3c9c2f7b8794440804478dc6e124e6c
Padmaja Jha (Microland) <Padmaja.Jha1@microland.com>
2023-01-09 14:21:39 +0530

    first commit

---
+++ src/pages/dashboard/DashboardOverview.js
@@ -0,0 +1,31 @@
+
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCashRegister, faChartLine, faCloudUploadAlt, faPlus, faRocket, faTasks, faUserShield } from '@fortawesome/free-solid-svg-icons';
+import { Col, Row, Button, Dropdown, ButtonGroup } from '@themesberg/react-bootstrap';
+
+import { DeviceWidget, TypeWidget } from "../../components/Widgets";
+import { PageVisitsTable } from "../../components/Tables";
+import { trafficShares, totalOrders } from "../../data/charts";
+
+export default () => {
+  return (
+    <> 
+    <DeviceWidget /> 
+    <TypeWidget /> 
+      <Row>
+        <Col xs={12} xl={12} className="mb-4">
+          <Row>
+            <Col xs={12} xl={12} className="mb-4">
+              <Row>
+                <Col xs={12} className="mb-4">
+                  <PageVisitsTable />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
